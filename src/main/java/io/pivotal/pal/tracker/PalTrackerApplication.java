c4c7b13b392c533f77d573f341f6c9a5b588afaf
pal_user <vycerae@gmail.com>
2020-02-26 13:39:09 -0700

    Completing the Database lab

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,15 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class PalTrackerApplication {
+public class PalTrackerApplication extends JdbcTimeEntryRepository{
+
+    public PalTrackerApplication(DataSource dataSource) {
+        super(dataSource);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
     @Bean
-    TimeEntryRepository timeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
